e712a9e9b53910e7f033c18193521b36f2165d3e
D-108 <72484574+Densentrated@users.noreply.github.com>
2023-09-18 23:36:27 -0700

    Tweaked some Animations and added a tests folder

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,34 +1,34 @@
 // header and subheadr movement
 anime({
-    targets: ".subHeader",
-    translateX: ["-40%", "4%"],
-    duration: 1500,
-    easeing: "easeInOutSine",
-    delay: anime.stagger(160)
-})
+  targets: ".subHeader",
+  translateX: ["-50%", "4%"],
+  duration: 1500,
+  easeing: "easeInOutSine",
+  delay: anime.stagger(160),
+});
 
 // navbar movement
 anime({
-    targets: ".navBar",
-    translateX: ["-10vw", "36vw"],
-    duration: 600,
-    easing: 'easeInOutSine'
-})
+  targets: ".navBar",
+  translateX: ["-100vw", "36vw"],
+  duration: 800,
+  easing: "easeInOutSine",
+});
 
 anime({
-    targets: ".hairlineSeparator",
-    translateX: ["-90vw", "10vw"],
-    duration: 600,
-    easing: 'easeInOutSine'
-})
+  targets: ".hairlineSeparator",
+  translateX: ["-90vw", "10vw"],
+  duration: 600,
+  easing: "easeInOutSine",
+});
 
 //smooth scroll on nav bar click
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
