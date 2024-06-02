0230141d10fd747c01c4541d063e24e065c74d49
D-108 <therealdensentrated@gmail.com>
2024-06-01 22:41:52 -0700

    Switched To Placeholder Website

---
+++ js/buttons.js
@@ -0,0 +1,9 @@
+var githubbutton = document.getElementById("github-button");
+githubbutton.addEventListener('click', function() {
+    window.location.href = "https://github.com/Densentrated"
+});
+
+var linkedinbutton = document.getElementById("linkedin-button");
+linkedinbutton.addEventListener("click", function() {
+    window.location.href = "https://www.linkedin.com/in/earl-chan-106427306/";
+})
